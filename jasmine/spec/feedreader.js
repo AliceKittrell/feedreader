699d1ac1eeaa978af155526fe694e1464f64d105
Shylmysten <TBoswell@kc.rr.com>
2018-06-14 11:28:39 -0500

    fixed logic in specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,15 +32,14 @@ $(function() {
          * and that the URL is not empty.
          */
          it('Feeds should have a URL defined and not be an empty string', function () {
-             // instantiate the variable i with a value of 0 so that we can increment it in the beforeEach method which will allow us to essentially loop through each element in the allFeeds array to test it
-             let i = 0;
-             beforeEach(function () {
-                 i++;
+             // loop through each element in the allFeeds array to test it
+             allFeeds.forEach(function (el) {
+                 // test for KEY - url
+                 expect(el.url).toBeDefined();
+                 // test that KEY - url is not an empty string
+                 expect(el.url).toBeTruthy();
              });
-             // test for KEY - url
-             expect(allFeeds[i].url).toBeDefined();
-             // test that KEY - url is not an empty string
-             expect(allFeeds[i].url).toBeTruthy();
+
          });
 
 
@@ -49,15 +48,14 @@ $(function() {
          * and that the name is not empty.
          */
          it('Feeds should have a name defined and not be an empty string', function () {
-             // instantiate the variable i with a value of 0 so that we can increment it in the beforeEach method which will allow us to essentially loop through each element in the allFeeds array to test it
-             let i = 0;
-             beforeEach(function () {
-                 i++;
+             // loop through each element in the allFeeds array to test it
+             allFeeds.forEach(function (el) {
+                 // test for KEY - name
+                 expect(el.name).toBeDefined();
+                 // test that KEY - name is not an empty string
+                 expect(el.name).toBeTruthy();
              });
-             // test for KEY - name
-             expect(allFeeds[i].name).toBeDefined();
-             // test that KEY - name is not an empty string
-             expect(allFeeds[i].name).toBeTruthy();
+
          });
     });
 
@@ -84,8 +82,6 @@ $(function() {
              // get Hamburger Menu from the DOM
              const menuIcon = document.getElementsByClassName('menu-icon-link')[0];
 
-             // TRUE if menu is hidden because it has the class menu-hidden
-             expect(document.body.classList.contains('menu-hidden')).toBe(true);
 
              // Trigger a click on the menu icon to set it state to visible
              $('.menu-icon-link').trigger('click');
@@ -112,18 +108,15 @@ $(function() {
 
          // wait for the feed to load before executing our test
          beforeEach(function (done) {
-             loadFeed(0, function () {
-                 done();
-             });
-
+             // load the first feed, then call done() to signal that the async operation has completed
+             loadFeed(0, done);
          });
 
-         it('should have at least one entry in the feed container after the Rss feed has loaded', function (done) {
+         it('should have at least one entry in the feed container after the Rss feed has loaded', function () {
              // grab the number of articles
              const numberOfEntries = $( '.feed' )[0].children.length;
              // test to insure there is at least one .entry-x element
              expect(numberOfEntries).not.toBe(0);
-             done();
          });
     });
 
@@ -134,30 +127,45 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 
+         let firstFeedHeaders = [];
+         let secondFeedHeaders = [];
 
-         // grab the innerText (Content) of the feed before the asyn function "loadFeed" finishes loading
-         const contentBeforeFeedLoads = $('.feed')[0].innerText;
-
-         // wait for the feed to load before executing our test
+         // The first feed loads, then completes its tasks and calls the second feed to load, completes its tasks, then uses the callback done to signal completion of all asynchronous operations
          beforeEach(function (done) {
-                loadFeed(0, function () {
-                done();
-             });
-         });
+            // when first feed loads, call the anonymous function to complete tasks
+            loadFeed(0,function () {
+                // loop through each .entry
+                document.querySelectorAll('.entry').forEach(function (el) {
+                    // grab the innerText and push it into array
+                    firstFeedHeaders.push(el.innerText);
+                });
+                // when tasks complete from first feed, call second feed, wait for it to load, then call the anonymous function to completes its tasks
+                loadFeed(1,function () {
+                    // loop through each .entry
+                    document.querySelectorAll('.entry').forEach(function (el) {
+                        // grab the innerTexxt and push it into array
+                        secondFeedHeaders.push(el.innerText);
+                    });
+                    // call done to signal that all asynchronous operations have completed
+                    done();
+                });
+            });
 
-         // insure that the .feed element is indeed empty before the Async function loadFeed completes, we could wait till after the load finishes, but it wouldn't matter, because we grabbed .feed's content before the feed completed anyways
-         it('should have no content before the feed loads', function () {
-              expect(contentBeforeFeedLoads).toBe('');
+
          });
 
 
          // wait till the feed loads to perform this test using the done parameter
-         it('should have new content after the feed loads', function (done) {
-             // now grab the content since the loadFeed function has completed
-             const contentAfterFeedLoads = $('.feed')[0].innerText;
-             // test insures the content before and after are not the same
-             expect(contentAfterFeedLoads).not.toEqual(contentBeforeFeedLoads);
-             done();
+         it('should have new content after the feed loads', function () {
+
+             // loop through each element in both arrays
+             for (var i = 0; i < 10; i++) {
+                 // The first 'heading' of the first feed should not be the same as the first 'heading' of the second feed
+                 expect(firstFeedHeaders[i]).not.toEqual(secondFeedHeaders[i]);
+             }
+
+
+
          });
     });
 }());
